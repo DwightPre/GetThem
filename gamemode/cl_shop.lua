17e2e787f319bbc01739217facc465ae937a0e85
DwightPre <31975207+DwightPre@users.noreply.github.com>
2017-09-22 04:46:11 +0200

    Prijzen gone
    srry :o

--- gamemode/cl_shop.lua
+++ gamemode/cl_shop.lua
@@ -400,114 +400,114 @@ sheet:AddSheet( "Entities", EntitiesTab, "icon16/user_orange.png" )
 local PistolButton = vgui.Create("DButton", weaponsTab)
 PistolButton:SetSize(100, 30)
 PistolButton:SetPos(10, 35)
-PistolButton:SetText("Pistol (800)")
+PistolButton:SetText("Pistol")
 PistolButton.DoClick = function() RunConsoleCommand("weapon_take", "pistol") WeaponFrame:Close() end --make it run our "weapon_take" console command with "pistol" as the 1st argument and then close the menu
 
 local SMGButton = vgui.Create("DButton", weaponsTab)
 SMGButton:SetSize(100, 30)
 SMGButton:SetPos(140, 35)
-SMGButton:SetText("SMG (1.000)")
+SMGButton:SetText("SMG")
 SMGButton.DoClick = function() RunConsoleCommand("weapon_take", "smg") WeaponFrame:Close() end
 
 local CRSBButton = vgui.Create("DButton", weaponsTab)
 CRSBButton:SetSize(100, 30)
 CRSBButton:SetPos(270, 35)
-CRSBButton:SetText("Crossbow (1.000)")
+CRSBButton:SetText("Crossbow")
 CRSBButton.DoClick = function() RunConsoleCommand("weapon_take", "crossbow") WeaponFrame:Close() end
 
 local ShotButton = vgui.Create("DButton", weaponsTab)
 ShotButton:SetSize(100, 30)
 ShotButton:SetPos(400, 35)
 
-ShotButton:SetText("Shotgun (1.000)")
+ShotButton:SetText("Shotgun")
 ShotButton.DoClick = function() RunConsoleCommand("weapon_take", "shotgun") WeaponFrame:Close() end
 
 local ARButton = vgui.Create("DButton", weaponsTab)
 ARButton:SetSize(100, 30)
 ARButton:SetPos(10, 70)
-ARButton:SetText("AR2(1.000)")
+ARButton:SetText("AR2")
 ARButton.DoClick = function() RunConsoleCommand("weapon_take", "ar2") WeaponFrame:Close() end
 
 local FragButton = vgui.Create("DButton", weaponsTab)
 FragButton:SetSize(100, 30)
 FragButton:SetPos(140, 70)
-FragButton:SetText("Frag(500)")
+FragButton:SetText("Frag")
 FragButton.DoClick = function() RunConsoleCommand("weapon_take", "frag") WeaponFrame:Close() end
 
 local AlyxButton = vgui.Create("DButton", weaponsTab)
 AlyxButton:SetSize(100, 30)
 AlyxButton:SetPos(270, 70)
-AlyxButton:SetText("Alyxgun(1200)")
+AlyxButton:SetText("Alyxgun(")
 AlyxButton.DoClick = function() RunConsoleCommand("weapon_take", "Alyxgun") WeaponFrame:Close() end
 
 -------------------------------------------------------------------
 local PistolAmmoButton = vgui.Create("DButton", ammoTab)
 PistolAmmoButton:SetSize(100, 30)
 PistolAmmoButton:SetPos(10, 35)
-PistolAmmoButton:SetText("Pistol Ammo (200)")
+PistolAmmoButton:SetText("Pistol Ammo")
 PistolAmmoButton:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton.DoClick = function() RunConsoleCommand("weapon_take", "pistolammo") WeaponFrame:Close() end
 
 local PistolAmmoButton2 = vgui.Create("DButton", ammoTab)
 PistolAmmoButton2:SetSize(100, 30)
 PistolAmmoButton2:SetPos(140, 35)
-PistolAmmoButton2:SetText("SMG Ammo (400)")
+PistolAmmoButton2:SetText("SMG Ammo")
 PistolAmmoButton2:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton2.DoClick = function() RunConsoleCommand("weapon_take", "smgammo") WeaponFrame:Close() end
 
 local PistolAmmoButton3 = vgui.Create("DButton", ammoTab)
 PistolAmmoButton3:SetSize(100, 30)
 PistolAmmoButton3:SetPos(270, 35)
-PistolAmmoButton3:SetText("Crossbow Ammo (400)")
+PistolAmmoButton3:SetText("Crossbow Ammo")
 PistolAmmoButton3:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton3.DoClick = function() RunConsoleCommand("weapon_take", "blotammo") WeaponFrame:Close() end
 
 local PistolAmmoButton4 = vgui.Create("DButton", ammoTab)
 PistolAmmoButton4:SetSize(100, 30)
 PistolAmmoButton4:SetPos(400, 35)
-PistolAmmoButton4:SetText("Shotgun Ammo (400)")
+PistolAmmoButton4:SetText("Shotgun Ammo")
 PistolAmmoButton4:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton4.DoClick = function() RunConsoleCommand("weapon_take", "shotgunammo") WeaponFrame:Close() end
 
 local PistolAmmoButton5 = vgui.Create("DButton", ammoTab)
 PistolAmmoButton5:SetSize(100, 30)
 PistolAmmoButton5:SetPos(10, 70)
-PistolAmmoButton5:SetText("AR2 Ammo (400)")
+PistolAmmoButton5:SetText("AR2 Ammo")
 PistolAmmoButton5:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton5.DoClick = function() RunConsoleCommand("weapon_take", "AR2ammo") WeaponFrame:Close() end
 
 local PistolAmmoButton6 = vgui.Create("DButton", ammoTab)
 PistolAmmoButton6:SetSize(100, 30)
 PistolAmmoButton6:SetPos(140, 70)
-PistolAmmoButton6:SetText("Alyxgun ammo(100)")
+PistolAmmoButton6:SetText("Alyxgun ammo")
 PistolAmmoButton6:SetTextColor( Color( 255, 0, 0, 255 ) )
 PistolAmmoButton6.DoClick = function() RunConsoleCommand("weapon_take", "Alyxgunammo") WeaponFrame:Close() end
 -------------------------------------------------------------------------------
 local FlashlightButton = vgui.Create("DButton", SpecialsTab)
 FlashlightButton:SetSize(100, 30)
 FlashlightButton:SetPos(10, 35)
-FlashlightButton:SetText("flashlight(100)")
+FlashlightButton:SetText("flashlight")
 FlashlightButton:SetTextColor( Color( 0, 0, 255, 255 ) )
 FlashlightButton.DoClick = function() RunConsoleCommand("weapon_take", "flashlight") WeaponFrame:Close() end
 
 local HPButton = vgui.Create("DButton", SpecialsTab)
 HPButton:SetSize(100, 30)
 HPButton:SetPos(140, 35)
-HPButton:SetText("150HP(300)")
+HPButton:SetText("150HP")
 HPButton:SetTextColor( Color( 0, 0, 255, 255 ) )
 HPButton.DoClick = function() RunConsoleCommand("weapon_take", "HP") WeaponFrame:Close() end
 
 local ArmorButton = vgui.Create("DButton", SpecialsTab)
 ArmorButton:SetSize(100, 30)
 ArmorButton:SetPos(270, 35)
-ArmorButton:SetText("Armor (500)")
+ArmorButton:SetText("Armor")
 ArmorButton:SetTextColor( Color( 0, 0, 255, 255 ) )
 ArmorButton.DoClick = function() RunConsoleCommand("weapon_take", "armor") WeaponFrame:Close() end
 
 local SprintButton = vgui.Create("DButton", SpecialsTab)
 SprintButton:SetSize(100, 30)
 SprintButton:SetPos(400, 35)
-SprintButton:SetText("Sprint (500)")
+SprintButton:SetText("Sprint")
 SprintButton:SetTextColor( Color( 0, 0, 255, 255 ) )
 SprintButton.DoClick = function() RunConsoleCommand("weapon_take", "sprint") WeaponFrame:Close() end
 -------------------------------------------------------------
@@ -515,42 +515,42 @@ SprintButton.DoClick = function() RunConsoleCommand("weapon_take", "sprint") Wea
 local Model1Button = vgui.Create("DButton", ModelTab)
 Model1Button:SetSize(100, 30)
 Model1Button:SetPos(10, 35)
-Model1Button:SetText("Gman (200)")
+Model1Button:SetText("Gman")
 Model1Button:SetTextColor( Color( 0, 0, 255, 255 ) )
 Model1Button.DoClick = function() RunConsoleCommand("weapon_take", "gman") WeaponFrame:Hide() end
 
 local Model2Button = vgui.Create("DButton", ModelTab)
 Model2Button:SetSize(100, 30)
 Model2Button:SetPos(140, 35)
-Model2Button:SetText("Cop (200)")
+Model2Button:SetText("Cop")
 Model2Button:SetTextColor( Color( 0, 0, 255, 255 ) )
 Model2Button.DoClick = function() RunConsoleCommand("weapon_take", "cop") WeaponFrame:Close() end
 
 local Model3Button = vgui.Create("DButton", ModelTab)
 Model3Button:SetSize(100, 30)
 Model3Button:SetPos(270, 35)
-Model3Button:SetText("Skeleton (200)")
+Model3Button:SetText("Skeleton")
 Model3Button:SetTextColor( Color( 0, 0, 255, 255 ) )
 Model3Button.DoClick = function() RunConsoleCommand("weapon_take", "skeleton") WeaponFrame:Close() end
 
 local Model4Button = vgui.Create("DButton", ModelTab)
 Model4Button:SetSize(100, 30)
 Model4Button:SetPos(400, 35)
-Model4Button:SetText("Phoenix (200)")
+Model4Button:SetText("Phoenix")
 Model4Button:SetTextColor( Color( 0, 0, 255, 255 ) )
 Model4Button.DoClick = function() RunConsoleCommand("weapon_take", "phoenix") WeaponFrame:Close() end
 
 local Model5Button = vgui.Create("DButton", ModelTab)
 Model5Button:SetSize(100, 30)
 Model5Button:SetPos(10, 70)
-Model5Button:SetText("Zombie (200)")
+Model5Button:SetText("Zombie")
 Model5Button:SetTextColor( Color( 0, 0, 255, 255 ) )
 Model5Button.DoClick = function() RunConsoleCommand("weapon_take", "zombie") WeaponFrame:Close() end
 
 local EntityButton = vgui.Create("DButton", EntitiesTab)
 EntityButton:SetSize(100, 30)
 EntityButton:SetPos(10, 35)
-EntityButton:SetText("HP Box (200)")
+EntityButton:SetText("HP Box")
 EntityButton:SetTextColor( Color( 0, 0, 255, 255 ) )
 EntityButton.DoClick = function() RunConsoleCommand("weapon_take", "Guard") WeaponFrame:Close() end
 
