8c4f69b559cbca7cc05dab133b9a9f78bed0fe64
DwightPre <31975207+DwightPre@users.noreply.github.com>
2017-09-26 00:58:42 +0200

    ammo crate fix

--- entities/entities/simple_ammo_crate/simple_ammo_crate.lua
+++ entities/entities/simple_ammo_crate/simple_ammo_crate.lua
@@ -1,5 +1,7 @@
 AddCSLuaFile("shared.lua")
 
+delay = 0
+maxtakes = 9
 
 function ENT:Initialize()
 self:SetModel("models/Items/ammocrate_smg1.mdl")
@@ -11,7 +13,16 @@ self:SetSolid( SOLID_VPHYSICS )
 end
 
 function ENT:Use(activator, caller)
-		caller:GiveAmmo(30,activator:GetActiveWeapon():GetPrimaryAmmoType())
+
+	if CurTime() < delay then return end
+	caller:GiveAmmo(30,activator:GetActiveWeapon():GetPrimaryAmmoType())
+	delay = CurTime() + 0.7
+	if maxtakes > 0 then
+	maxtakes = maxtakes	-1
+	else
+	self:Remove() 
+	maxtakes = 9
+	end
 end
 
 list.Set( "NPC", "simple_ammo_crate", {
