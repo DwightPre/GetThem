6def7eb4f66872f6dcc07d7373b57d7fd01203f4
DwightPre <31975207+DwightPre@users.noreply.github.com>
2017-10-06 04:33:37 +0200

    BreakabkeProp Update
    Now you can upgrade HP of breakable prop with USE-key. & Remove when full health

--- entities/entities/gt_breakable_prop/init.lua
+++ entities/entities/gt_breakable_prop/init.lua
@@ -34,6 +34,10 @@ function ENT:Use(activator, caller)
 if self:Health() < (self.StartHealth) and caller:GetActiveWeapon():GetClass() == "gt_builder" then
 self:SetHealth(self:Health() +20)
 --delay = CurTime() + 0.3
+else
+if self:Health() == (self.StartHealth) and caller:GetActiveWeapon():GetClass() == "gt_builder" then
+self:Remove() // for now..
+end
 end
 end
 
