289ab56bcf9b2b11a44114e1ea916250f42e472a
DwightPre <31975207+DwightPre@users.noreply.github.com>
2017-10-06 04:43:19 +0200

    CleanHud_BreakableProp
    Now with 20% less lag :smiley:

--- entities/entities/gt_breakable_prop/cl_init.lua
+++ entities/entities/gt_breakable_prop/cl_init.lua
@@ -13,15 +13,15 @@ function ENT:Draw()
 	Ang:RotateAroundAxis(Ang:Up(), 90)
 
 	cam.Start3D2D(Pos + Ang:Up() * 28, Ang, 0.090)
-	surface.SetDrawColor( 0, 0, 0, 150 )
-	surface.DrawRect( -300, -100, 600, 178 )
-	surface.SetDrawColor( Color( 229,228,55 ))
-	surface.DrawOutlinedRect( -300, -100, 600, 178 )
+	--surface.SetDrawColor( 0, 0, 0, 150 )
+	--urface.DrawRect( -300, -100, 600, 178 )
+	--surface.SetDrawColor( Color( 229,228,55 ))
+	--surface.DrawOutlinedRect( -300, -100, 600, 178 )
 
 
 	draw.SimpleText( "Prop Health", "DermaLarge", 0, -55, Color( 255,255,255 ), 1, 0 )
 	draw.SimpleText( "".. tostring(height) , "DermaLarge", 0, -10, Color( 255,255,255 ), 1, 0 )
-	draw.RoundedBox( 12, -270, 20, height *1, 20,  Color( 229,228,55 ) ) 
+	draw.RoundedBox( 12, -130, 20, height /2, 20,  Color( 229,228,55 ) ) 
 	cam.End3D2D()
 
 	end
